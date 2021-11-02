280f96b1ec4cfa845c811ad21aba80be150af518
Caleb Wang <v-calewang@microsoft.com>
2021-11-02 22:50:44 +0800

    added github actions to automatically push build files to gh-pages branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World. This is Caleb's first official website!! 
         </p>
         <a
           className="App-link"
